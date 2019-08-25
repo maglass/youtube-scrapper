998139c551df895419cc14eb688df8f17ceea0d9
drake-jin <dydwls121200@gmail.com>
2019-08-25 21:35:15 +0900

    module이란녀석으로 재 정의해서 사용할 예정

--- src/how.to.use.youtube.api.js
+++ src/how.to.use.youtube.api.js
@@ -1,6 +1,6 @@
 const DBClient = require('lib/dbclient')
 const OAuth = require('lib/oauth')
-const Youtube = require('lib/youtube')
+const Youtube = require('module/youtube')
 
 async function app () {
   const dbClient = new DBClient()
@@ -21,22 +21,28 @@ async function app () {
   const youtube = new Youtube(accessToken)
 
   const channelInfo = await youtube.getChannel('UCdtRAcd3L_UpV4tMXCw63NQ')
-  console.log(channelInfo)
+  // console.log(channelInfo)
+  console.log(JSON.stringify(channelInfo))
 
   const videoList = await youtube.getVideoListFromChannel('UCdtRAcd3L_UpV4tMXCw63NQ')
-  console.log(videoList)
+  // console.log(videoList)
+  console.log(JSON.stringify(videoList))
 
   const video = await youtube.getVideo('SKilLXN_isw') // 자막 없는녀석
   // const video = await youtube.getVideo('40dJS_LC6S8') // 자막 있는녀석
-  console.log(video)
+  // console.log(video)
+  console.log(JSON.stringify(video))
 
   const captions = await youtube.getCaptions('SKilLXN_isw') // 자막 없는녀석 한국말
   // const captions = await youtube.getCaptions('40dJS_LC6S8') // 자막 있는녀석 외국말
-  console.log(captions)
+  // console.log(captions)
+  console.log(JSON.stringify(captions))
 
   const downloadCaption = await youtube.getCaptionObject('UIiUWVPvyby5kSDfusul30TsRbEVXN5fTeTtehtgMqo=') // 자막 없는녀석의 자동 한글 자막
   // const downloadCaption = await youtube.getCaptionObject('4TDrrCVZYdY0fELrxbuSvNX3OiTMoM9N') // 자막 있는녀석의 외국말인데 한글 번역 자막
   console.log(downloadCaption)
+
+  process.exit(0)
 }
 
 /*

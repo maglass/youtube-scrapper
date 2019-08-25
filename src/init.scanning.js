074b0d1cfad1bc5a27ba8a5f6fe19b27e4d9815f
drake-jin <dydwls121200@gmail.com>
2019-08-25 20:11:09 +0900

    index.js 파일 변경, 해당 프로젝트의 소스코드는 여러개가 실행될 예정

---
+++ src/init.scanning.js
@@ -0,0 +1,14 @@
+/**
+ * @Title 최초에 등록한 유투브 채널들을 전부 스캔해옵니다.
+ * @Description 데이터베이스에 등록된 channel_id 들을 이용하여 captionObject까지의 모든 정보들을 받아올 수 있도록 합니다.
+ */
+
+const app = async () => {
+  console.log('hello')
+
+
+  process.exit(0)
+}
+
+
+app()
